6839899e92945256067d401ea367f75dedf08a49
arshiamonem <a360ar@yahoo.com>
2025-04-21 17:20:02 +0330

    post category model created
    I have created the post category model

--- devsocial/blog/models.py
+++ devsocial/blog/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Test(models.Model):
     title = models.CharField(max_length=100)
     
+
+class PostCategory(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title}"
--- devsocial/blog/post_category.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class PostCategory(models.Model):
-    title = models.CharField(max_length=511)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.title}"
