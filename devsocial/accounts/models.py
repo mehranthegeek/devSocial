48a1cbc57c122525a41fe733d03ec67b2e17c701
sepehr <hunter.ghostlead@gmail.com>
2025-05-05 00:38:09 +0330

    cusrom user model has been created

--- devsocial/accounts/models.py
+++ devsocial/accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
 
 class CustomUserRole(models.Model):
     role = models.CharField(max_length=50)
@@ -6,3 +8,16 @@ class CustomUserRole(models.Model):
 
     def __str__(self):
         return self.role
+    
+
+class CustomUser(AbstractUser):
+    role = models.ForeignKey(CustomUserRole, on_delete=models.SET_NULL, null=True , blank = True)
+
+    def is_superuser(self):
+        return self.role and self.role.name == 'superuser'
+
+    def can_delete_posts(self):
+        return self.role and self.role.name in ['superuser', 'moderator']
+
+    def can_create_posts(self):
+        return self.role and self.role.name in ['superuser', 'moderator', 'user']
