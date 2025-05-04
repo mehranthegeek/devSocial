73fd89aaf5160386e91457c362905c2945e77509
yasamanght <ghattanyasaman1381@gmail.com>
2025-05-05 01:12:44 +0330

    Post model modified a little

--- devsocial/blog/models.py
+++ devsocial/blog/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)  
     content = models.TextField() 
-    category = models.ForeignKey(
-        'Category', on_delete=models.CASCADE, related_name='posts'
-    )
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
-    ) 
+    # category = models.ForeignKey(
+    #     'Category', on_delete=models.CASCADE, related_name='posts'
+    # )
+    # author = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='posts'
+    # ) 
     created_at = models.DateTimeField(auto_now_add=True) 
     updated_at = models.DateTimeField(auto_now=True)  
 
