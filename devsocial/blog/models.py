59193d9d49287499a0d65f92b7752e99e1aae9df
yasamanght <ghattanyasaman1381@gmail.com>
2025-05-05 00:48:00 +0330

    post model created

--- devsocial/blog/models.py
+++ devsocial/blog/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Test(models.Model):
-    title = models.CharField(max_length=100)
+class Post(models.Model):
+    title = models.CharField(max_length=255)  
+    content = models.TextField() 
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE, related_name='posts'
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts'
+    ) 
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return self.title
+
     
