4c107dbc4f3d752af2cfa899d80326058be6a3ab
yasamanght <ghattanyasaman1381@gmail.com>
2025-06-01 21:53:59 +0330

    Added Final post Model

--- devsocial/blog/urls.py
+++ devsocial/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views 
+from .views import profile
 
 urlpatterns = [
     path('test/',views.TestPage , name='test'),
+    path('profile/', profile, name='profile'),
 ]
--- devsocial/blog/views.py
+++ devsocial/blog/views.py
@@ -5,3 +5,10 @@ def TestPage(request):
     testObjects = Test.objects.all()
     return render(request, 'blog/testPage.html', {'testObjects':testObjects} )
 
+@login_required
+def profile(request):
+    user_posts = request.user.posts.all().order_by('-created_at')
+    paginator = Paginator(user_posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/profile.html', {'page_obj': page_obj})
